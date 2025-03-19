eab04c6eb100f098e4374cd061c51b060aaf9e00
Gutz <unaiclapers@inspladelestany.cat>
2025-03-19 16:02:13 +0100

    LLibre + Biblio 0.1

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -1,2 +1,11 @@
+import java.util.ArrayList;
+
 public class Biblioteca {
+
+    ArrayList<Usuari> usuaris = new ArrayList<>();
+    ArrayList<Llibre> llibres = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+    }
 }
--- src/Llibre.java
+++ src/Llibre.java
@@ -1,2 +1,47 @@
 public class Llibre {
+    String titol;
+    String codi;
+    String seccio;
+    int stockTotal;
+    boolean disponible = true;
+
+    public String getTitol() {
+        return titol;
+    }
+
+    public void setTitol(String titol) {
+        this.titol = titol;
+    }
+
+    public String getCodi() {
+        return codi;
+    }
+
+    public void setCodi(String codi) {
+        this.codi = codi;
+    }
+
+    public String getSeccio() {
+        return seccio;
+    }
+
+    public void setSeccio(String seccio) {
+        this.seccio = seccio;
+    }
+
+    public int getStockTotal() {
+        return stockTotal;
+    }
+
+    public void setStockTotal(int stockTotal) {
+        this.stockTotal = stockTotal;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
 }
