d3a408b1f751dc9bcee7b4218129fea12cb1bf81
color <eulercolorado@inspladelestaby.cat>
2025-03-25 14:54:07 +0100

    BIBLIO V0

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -1,2 +1,22 @@
-public class Biblioteca {
+package gestio;
+
+import model.Usuari;
+import model.Publicacio;
+
+public class Main {
+    public static void main(String[] args) {
+        Biblioteca biblioteca = new Biblioteca();
+
+        Usuari usuari1 = new Usuari("Joan");
+        Usuari usuari2 = new Usuari("Maria");
+
+        Publicacio publicacio1 = new Publicacio("El Quijote");
+        Publicacio publicacio2 = new Publicacio("Cien Años de Soledad");
+
+        biblioteca.afegirUsuari(usuari1);
+        biblioteca.afegirUsuari(usuari2);
+
+        biblioteca.afegirPublicacio(publicacio1);
+        biblioteca.afegirPublicacio(publicacio2);
+    }
 }
