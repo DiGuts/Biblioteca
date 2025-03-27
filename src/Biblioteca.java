b8532a06061ff66dd4a53b9f69150a1d70978406
Gutz <113361940+DiWolf15@users.noreply.github.com>
2025-03-27 15:04:33 +0100

    Add "Reserva" + Modificacions importants a bilblioteca

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -8,9 +8,41 @@ public class Biblioteca {
     private static Usuari actualUser;
 
     public static void main(String[] args) {
+        menuLogin();
+        menuReservar();
+    }
+    private static int registerUser() {
+        try {
+            Usuari u = new Usuari(input("dni: "),input("nom: "),input("telefon: "),input("dirrecio: "), input("passwd: "));
+            usuaris.add(u);
+            actualUser = u;
+            return 1;
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
+    }
 
-        boolean sortir = false;
-        while (!sortir) {
+    private static int loginUser() {
+        try {
+            String usr = input("dni usuari: ");
+            for (Usuari u : usuaris) {
+                if (u.getDni().equals(usr)) {
+                    if (u.getPasswd().equals(input("passwd: "))) { actualUser = u; }
+                }
+            }
+            return 1;
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
+    }
+
+    private static void menuLogin() {
+        boolean sessioIniciada = false;
+        while (!sessioIniciada) {
             System.out.println("\n--- Menú Biblioteca ---");
             System.out.println("1. Registrar usuari");
             System.out.println("2. Iniciar sessió");
@@ -19,27 +51,40 @@ public class Biblioteca {
             String opcio = input("Escull una opció: ");
             switch (opcio) {
                 case "1":
-                    registerUser();
+                    if (registerUser() == 1){
+                        sessioIniciada = true;
+                    }
                     break;
                 case "2":
-                    loginUser();
+                    if (loginUser() == 1){
+                        sessioIniciada = true;
+                    }
                     break;
                 case "0":
                     System.exit(0);
             }
         }
     }
-    private static void registerUser() {
-        Usuari u = new Usuari(input("dni: "),input("nom: "),input("telefon: "),input("dirrecio: "), input("passwd: "));
-        usuaris.add(u);
-        actualUser = u;
-    }
 
-    private static void loginUser() {
-        String usr = input("dni usuari: ");
-        for (Usuari u : usuaris) {
-            if (u.getDni().equals(usr)) {
-                if (u.getPasswd().equals(input("passwd: "))) { actualUser = u; }
+    private static void menuReservar() {
+        boolean menu = true;
+        while (menu) {
+            System.out.println("\n--- Menú Biblioteca ---");
+            System.out.println("1. Reservar llibre");
+            System.out.println("2. Consultar reserves");
+            //si es admin o bibliotecari hauria de printejar mes opcions.
+            System.out.println("0. Sortir");
+
+            String menuOpcio = input("Escull una opció: ");
+            switch (menuOpcio) {
+                case "1":
+                    //Reservar Llibre
+                    break;
+                case "2":
+                    //Consultar reserves fetes.
+                    break;
+                case "0":
+                    System.exit(0);
             }
         }
     }
---
+++ src/Reserva.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Reserva {
+    private Llibre llibre;
+    private Usuari usuari;
+    private Date dataInici;
+    private Date dataFi;
+}
